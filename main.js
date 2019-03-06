1a89b41b85ec9d393d06ce7fc00731a4278a435b
Marsb10 <sebastian.mariano.ulloa@alumnos.upm.es>
2019-03-06 23:26:42 +0100

    Boton a top añadido

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("up").style.display = "block";
+  } else {
+    document.getElementById("up").style.display = "none";
+  }
+}
+
+function Gototop() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0 
+  
+} 
